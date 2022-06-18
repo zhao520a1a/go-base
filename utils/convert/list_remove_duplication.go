39dda330580ac5f0ab6b5e0b0598723ba524f8f7
zhaojinxin.zjx <zhaojinxin.zjx@alibaba-inc.com>
2022-06-18 12:02:57 +0800

    refator: string encode

--- utils/convert/list_remove_duplication.go
+++ utils/convert/list_remove_duplication.go
@@ -4,9 +4,7 @@ import (
 	"reflect"
 )
 
-/*
-去除slice和list的重复元素，非常有用的功能
-*/
+// Duplicate 去除slice和list的重复元素，非常有用的功能
 func Duplicate(a interface{}) (ret []interface{}) {
 	va := reflect.ValueOf(a)
 	for i := 0; i < va.Len(); i++ {
