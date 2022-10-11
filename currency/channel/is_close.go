2594e9fa77c57ff9553634a94d2315343ceb489e
zhaojinxin.zjx <zhaojinxin.zjx@alibaba-inc.com>
2022-10-11 17:21:26 +0800

    chore: channel is close annotation

--- currency/channel/is_close.go
+++ currency/channel/is_close.go
@@ -1,5 +1,6 @@
 /*
 协程的同步： 关闭通道-测试阻塞通道
+channel 关闭后失去了阻塞的功能，还是可以被读取<-channel（只是返回数据为空值+不可读状态）；
 */
 package main
 
@@ -13,25 +14,26 @@ func main() {
 
 func getData(ch chan int) {
 
-	//使用for range读channel
-	//for i := range ch {
+	// 使用for range读channel
+	// for i := range ch {
 	//	fmt.Println(i)
-	//}
+	// }
 
-	//使用_,ok判断channel是否关闭
+	// 使用_,ok判断channel是否关闭
 	for {
 		if i, ok := <-ch; ok {
 			fmt.Println(i)
 		} else {
+			fmt.Println(<-ch) // 关闭后还是可以被读取
 			fmt.Println("receive close chan msg")
 			break
 		}
 	}
 
 	// 不判断 chanel 关闭会死循环
-	//for {
+	// for {
 	//	fmt.Println(<-ch)
-	//}
+	// }
 
 }
 
